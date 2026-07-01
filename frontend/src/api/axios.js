34cbcec052d8dd0925f1c5514cbf1de32c6b8c27
Abhishek Kumar <abhisekyadav03017@gmail.com>
2026-07-01 14:36:03 +0530

    Fix API URL and Vercel routing

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://school-management-backend-ndv4.onrender.com",
+  baseURL: "https://school-management-backend-ndv4.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
